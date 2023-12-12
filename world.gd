6d011138a810ad53abecb8c560ff4b295a019419
joferrer5 <joferrer@s.sfusd.edu>
2023-12-11 17:57:38 -0800

    Completed Progress Check #1

--- world.gd
+++ world.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
+@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
+@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	RenderingServer.set_default_clear_color(Color.BLACK)
+	polygon_2d.polygon = collision_polygon_2d.polygon
