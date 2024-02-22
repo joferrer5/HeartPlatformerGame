2a867fe59d358336eea21190b8683588f76f038b
joferrer5 <121971176+joferrer5@users.noreply.github.com>
2024-02-22 14:35:13 -0800

    Progress Check 5 20:07

--- player.gd
+++ player.gd
@@ -47,7 +47,7 @@ func handle_jump():
 	if is_on_floor() or Coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = movement_data.jump_velocity
-	if not is_on_floor():
+	elif not is_on_floor():
 		if Input.is_action_just_released("ui_up") and velocity.y < movement_data.jump_velocity / 2:
 			velocity.y = movement_data.jump_velocity / 2
 			
@@ -82,3 +82,6 @@ func update_animations(input_axis):
 		
 	if not is_on_floor():
 		animated_sprite_2d.play("jump")
+
+func _on_hazard_detector_area_entered(area):
+	queue_free()
